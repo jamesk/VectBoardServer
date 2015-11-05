6f281e1e8f03921660cf66819e292e8d4a09edcb
James Kearney <james@movio.co>
2015-11-05 21:44:30 +1300

    master: Updated with neater code and original image on page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -28,88 +28,71 @@ object Application extends Controller {
   def index() = Action {
     Ok(views.html.Application.index.render())
   }
-  
-  def upload(qqfile: String, greyThreshold: Double, highpassFilter: Int, turd: Double) = Action { request =>
-    //var moveTo: FileOutputStream = null
 
+  def upload(qqfile: String, greyThreshold: Double, highpassFilter: Int, turd: Double) = Action { request ⇒
     Logger.info(s"Name of the file $qqfile")
-    // Another way I used to grab the name of the file
-    val filename = qqfile
+    val data = request.body.asRaw.map(r ⇒ r.asFile).get //TODO: fix get
+    val file = getFile(qqfile)
+    Logger.info(s"Copying to ${file.getAbsolutePath}");
+    FileUtils.copyFile(data, file)
 
-    Logger.info(s"Absolute on where to send ${Play.getFile("").getAbsolutePath() + File.separator + "uploads" + File.separator}");
-    //try {
-      val data = request.body.asRaw.map(r => r.asFile).get //TODO: fix get
-
-      val file = getFile(filename)
-      
-      FileUtils.copyFile(data, file)
-      
-      val imageData = boofThings(file)
-      
-      val outputPath = processImage(file, greyThreshold, highpassFilter, turd)
-      /*
-      moveTo = new FileOutputStream(file)
-
-      IOUtils.copy(data, moveTo)
-*/
-      //conversion here using file 
-      
-    /*} catch {
-      case ex: Throwable ⇒
-        // catch file exception
-        // catch IO Exception later on
-        //renderJSON("{success: false}")
-        
-    }*/
-
-    //renderJSON("{success: true}")
-    Ok(Json.obj(
-      "outputPath" -> JsString(routes.Application.getUpload(filename + ".svg").path),
-      "data" -> JsString(Source.fromFile(outputPath).mkString),
-      "imageDetails" -> Json.toJson(imageData)
-    ))
+    val jsonResponse = processFile(file, greyThreshold, highpassFilter, turd)
+    Ok(jsonResponse)
   }
 
   def boofThings(file: File): ImageDetails = {
     val image = boofcv.io.image.UtilImageIO.loadImage(file.getAbsolutePath)
-    
+
     ImageDetails(image.getWidth, image.getHeight)
   }
-  
-  private def getFile(filename: String) = 
+
+  private def getFile(filename: String) =
     new File(Play.application.getFile("").getAbsolutePath() + File.separator + "uploads" + File.separator + filename)
-  
+
+  private def processFile(file: File, greyThreshold: Double, highpassFilter: Int, turd: Double) = {
+    val imageData = boofThings(file)
+
+    val outputPath = processImage(file, greyThreshold, highpassFilter, turd)
+
+    Json.obj(
+      "outputPath" -> JsString(routes.Application.getUpload(file.getName + ".svg").path),
+      "data" -> JsString(Source.fromFile(outputPath).mkString),
+      "imageDetails" -> Json.toJson(imageData),
+      "original" -> JsString(routes.Application.getUpload(file.getName).path)
+    )
+  }
+
   private def processImage(file: File, greyThreshold: Double = 0.48, highpassFilter: Int = 10, turd: Double = 1) = {
     val outputPath = s"${file.getAbsolutePath}.svg"
-    
+
     val imageData = boofThings(file)
-      
+
     val turdConstant = 0.00002
     val area = imageData.width * imageData.height
     val baseTurd = turdConstant * area
-    
+
     val finalTurd = (turd * baseTurd).toInt
-    
+
     //TODO: protect against injection attacks
     val pipelineCmd = s"convert ${file.getAbsolutePath} bmp:-" #| s"mkbitmap -b 1 -x -f $highpassFilter -t $greyThreshold -o -" #| s"potrace --svg --tight -t $finalTurd -o $outputPath"
     Logger.info(s"Running pipeline with command: $pipelineCmd")
-    
+
     pipelineCmd.!!
-    
+
     outputPath
   }
-  
+
   def reprocess(filename: String, greyThreshold: Double, highpassFilter: Int, turd: Double) = Action {
     val file = getFile(filename)
+
+    val jsonResponse = processFile(file, greyThreshold: Double, highpassFilter: Int, turd: Double)
     
-    val outputPath = processImage(file, greyThreshold, highpassFilter, turd)
-    
-    Ok(Json.obj("data" -> JsString(Source.fromFile(outputPath).mkString)))
+    Ok(jsonResponse)
   }
-  
-  def getUpload(filename: String) = Action { request =>
+
+  def getUpload(filename: String) = Action { request ⇒
     val fileToServe = getFile(filename)
-    
+
     if (fileToServe.exists)
       Ok.sendFile(fileToServe, inline = true)
     else
